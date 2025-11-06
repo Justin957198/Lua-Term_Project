43a068056bc8a7491f07438542559da648ed3b1c
Denali Miao <yaye@Denalis-MacBook-Air.local>
2025-11-06 11:58:11 -0500

    feat: Added player movement module to Main

--- Main.lua
+++ Main.lua
@@ -1,7 +1,7 @@
 local terrain_module = require("level_generator")
+local movement_module = require("movement_controls")
 
 print("Sample Terrain making tool") -- temp
-
 
 local numleng
 local numdep
@@ -15,7 +15,31 @@ local length = tonumber(numleng)
 
 --print(depth .. " and " .. length)
 
-terrain_module.terrain_generator(depth, length)
+local maze = terrain_module.terrain_generator(depth, length)
 terrain_module.bomb_generator()
 terrain_module.treasure_generator()
 terrain_module.terrain_display()
+
+-- Player Starting Position
+local player_x, player_y = 1, 1
+maze[player_x][player_y] = "P"
+
+while true do
+    os.execute("clear") -- clear terminal
+
+    terrain_module.terrain_display()
+    print("\nUse w/a/s/d to move:")
+    local move = io.read()
+
+    maze[player_x][player_y] = "?"
+    if move == "w" then
+        player_x, player_y = movement_module.move_up(player_x, player_y, maze)
+    elseif move == "s" then
+        player_x, player_y = movement_module.move_down(player_x, player_y, maze)
+    elseif move == "a" then
+        player_x, player_y = movement_module.move_left(player_x, player_y, maze)
+    elseif move == "d" then
+        player_x, player_y = movement_module.move_right(player_x, player_y, maze)
+    end
+    maze[player_x][player_y] = "P"
+end
--- movement_controls.lua
+++ movement_controls.lua
@@ -3,11 +3,14 @@ local movement_m = {}
 
 function movement_m.move_up(x_p, y_p, maze)
     local x_value = x_p
-    local y_value = x_p
+    local y_value = y_p
 
-    if maze[x_value - 1][y_value] == nil then
+    if maze[x_value - 1][y_value] == nil or maze[x_value - 1][y_value] == nil then
         io.write("***Out of bounds***\n Player still on " .. x_p .. "," .. y_p)
         return x_p, y_p
+    elseif maze[x_value - 1][y_value] == 0 then
+        io.write("you hit a bomb! Game over!\n")
+        os.exit()
     else
         x_p = x_p -1
         return x_p, y_p
@@ -17,11 +20,14 @@ end
 
 function movement_m.move_down(x_p, y_p, maze)
     local x_value = x_p
-    local y_value = x_p
+    local y_value = y_p
 
-    if maze[x_value + 1][y_value] == nil then
+    if maze[x_value + 1] == nil  or maze[x_value + 1][y_value] == nil then
         io.write("***Out of bounds***\n Player still on " .. x_p .. "," .. y_p)
         return x_p, y_p
+    elseif(maze[x_value + 1][y_value] == 0) then
+        io.write("You hit a bomb! Gave over!\n")
+        os.exit()
     else
         x_p = x_p + 1
         return x_p, y_p
@@ -30,11 +36,14 @@ end
 
 function movement_m.move_left(x_p, y_p, maze)
     local x_value = x_p
-    local y_value = x_p
+    local y_value = y_p
 
     if maze[x_value][y_value - 1] == nil then
         io.write("***Out of bounds***\n Player still on " .. x_p .. "," .. y_p)
         return x_p, y_p
+    elseif maze[x_value][y_value - 1] == 0 then
+        io.write("You hit a bomb! Game Over!\n")
+        os.exit()
     else
         y_p = y_p - 1
         return x_p, y_p
@@ -43,11 +52,14 @@ end
 
 function movement_m.move_right(x_p, y_p, maze)
     local x_value = x_p
-    local y_value = x_p
+    local y_value = y_p
 
     if maze[x_value][y_value + 1] == nil then
         io.write("***Out of bounds***\n Player still on " .. x_p .. "," .. y_p)
         return x_p, y_p
+    elseif maze[x_value][y_value + 1] == 0 then
+        io.write("You hit a bomb! Gamne Over!\n")
+        os.exit()
     else
         y_p = y_p + 1
         return x_p, y_p
